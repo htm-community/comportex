9af5dd11ad1b9971eaee81e2190288c1576d7878
Felix Andrews <felix@nfrac.org>
2014-11-13 13:09:56 +0800

    v0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.nfrac/comportex "0.0.6-SNAPSHOT"
+(defproject org.nfrac/comportex "0.0.6"
   :description "Functionally composable cortex, an implementation of Hierarchical Temporal Memory"
   :url "http://github.com/nupic-community/comportex/"
   :license {:name "Eclipse Public License"
