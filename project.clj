c5530483f53a3bca6003f5c23ffcd075edd086b5
Felix Andrews <felix@nfrac.org>
2015-12-03 15:34:14 +0800

    bump version

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject org.nfrac/comportex "0.0.12-SNAPSHOT"
+(defproject org.nfrac/comportex "0.0.13-SNAPSHOT"
   :description "Functionally composable cortex, an implementation of Hierarchical Temporal Memory"
   :url "http://github.com/nupic-community/comportex/"
   :license {:name "GNU Affero General Public Licence"
             :url "http://www.gnu.org/licenses/agpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/data.int-map "0.1.0"]
-                 [org.clojure/test.check "0.8.2"]
-                 [clj-http "1.1.1"]
-                 [cljs-http "0.1.30"]]
+  :dependencies [[org.clojure/clojure "1.8.0-RC2"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/data.int-map "0.2.1"]
+                 [org.clojure/test.check "0.9.0"]
+                 [clj-http "2.0.0"]
+                 [cljs-http "0.1.38"]]
 
   :jvm-opts ^:replace ["-server" "-Xmx2500m"]
 
@@ -16,7 +16,7 @@
                         :compiler {:output-to "target/testable.js"
                                    :optimizations :advanced}}]}
 
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.7.107"]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.7.189"]
                                   [criterium "0.4.3"]]
                    :plugins [[lein-cljsbuild "1.1.0"]
                              [com.cemerick/clojurescript.test "0.3.3"]
